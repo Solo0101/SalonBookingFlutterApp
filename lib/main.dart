0ad1c9790a4902f85cfdc009a05cf4559422bbe3
Adrian Solomon <adrian.solomon@student.upt.ro>
2022-06-17 01:00:50 +0300

    OS UI top NavBar Transparent background

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_project/Constants/router_constants.dart';
@@ -14,6 +15,9 @@ SharedPreferences themePrefs = SharedPreferences as SharedPreferences;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent));
   themePrefs = await SharedPreferences.getInstance();
   await Firebase.initializeApp();
   deletePastAppointmentsFromDb();
